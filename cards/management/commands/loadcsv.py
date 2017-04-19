8c65f5ce7b57c550401a580a58ee6bf35b866d0a
Daniel Ohayon <daniel@genestack.com>
2017-04-19 22:40:26 +0100

    generalise dataloading script

--- cards/management/commands/loadcsv.py
+++ cards/management/commands/loadcsv.py
@@ -1,19 +1,9 @@
-import re
 import csv
 
 from django.core.management.base import BaseCommand
 from cards.models import Card
 
 # use `python manage.py flush` to clear database
-
-
-def remove_tags(raw_html):
-    # replace div tags by newline
-    raw_html = raw_html.replace('<div>', '\n')
-    raw_html = raw_html.replace('&nbsp;', ' ')
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', raw_html)
-    return cleantext
 
 
 class Command(BaseCommand):
@@ -28,8 +18,7 @@ class Command(BaseCommand):
             next(reader)
             for i, row in enumerate(reader, 1):
                 self.stdout.write("Processing row %d..." % i)
-                row = [remove_tags(e) for e in row]
                 card = Card()
-                card.question = row[1]
-                card.answer = row[0]
+                card.question = row[0]
+                card.answer = row[1]
                 card.save()
